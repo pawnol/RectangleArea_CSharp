8efb540ad4eab4c297c735e76bbe5868d3aeb27a
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-12-06 20:14:31 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Rectangle Area
+ * Pawelski
+ * 12/6/2021
+ * Read the code and run the program serveal times so that you understand
+ * how the program currently works. Once you understand the code, modify
+ * program so that is also calcualtes the perimeter of a rectnagle with the 
+ * length and width given by the user. Lastly, answer any questions on the
+ * activity sheet.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables.
+            int length;
+            int width;
+            int area;
+
+            // Input length and width.
+            Console.Write("Please enter the length of the rectangle >> ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the length of the rectangle >> ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate and display area.
+            area = length * width;
+            Console.WriteLine("The area of the rectangle is " + area + ".");
+
+            // Add your code here!
+            
+        }
+    }
+}
